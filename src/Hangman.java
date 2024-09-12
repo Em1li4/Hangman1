062c4f37ef7f944977f208c5136a9b47bd02d600
Natalia Rodriguez Giraldo <em1li4gilo@gmail.com>
2024-09-12 11:27:50 -0700

    added boolean to printWordState to check if the user has the right count of letters in the word, in such case user wins, added break to stop the system after the user wins. Added comments to explain what each part of the code does.

--- src/Hangman.java
+++ src/Hangman.java
@@ -6,8 +6,60 @@ import java.util.Scanner;
 import java.io.File;
 
 public class Hangman {
-    public static void main(String[] args) {
-        System.out.print("Hello and welcome!");
-trial
+    public static void main(String[] args) throws FileNotFoundException {
+
+        // Scanner for file of words
+        Scanner file = new Scanner(new File("C:\\Users\\em1li\\OneDrive\\Documents\\EnglishWordsForHangmanGame\\words_alpha.txt"));
+       // Scanner for user input
+        Scanner keyboard = new Scanner(System.in);
+
+        // Create list to store words
+        List<String> wordsList = new ArrayList<String>();
+
+        // Fill words in the ArrayList
+        while (file.hasNext()) {
+            wordsList.add(file.nextLine());
+        }
+
+        Random rand = new Random();
+        // Get a random word within the range of the size of words list, nested method calls
+        String word = wordsList.get(rand.nextInt(wordsList.size()));
+        System.out.println(word);
+
+        // List for the guesses the player has made
+        List<Character> playerGuesses = new ArrayList<>();
+        printWordState(word, playerGuesses);
+
+        while (true) {
+            getPlayerGuesses(keyboard, playerGuesses, word);
+            if (printWordState(word, playerGuesses)) {
+                break;
+            }
+        }
+        System.out.println("You win!");
+    }
+
+    private static void getPlayerGuesses(Scanner keyboard, List<Character> playerGuesses, String word) {
+        System.out.println("Please enter a letter");
+        // read user input
+        String letterGuess = keyboard.nextLine();
+        // add character of user input and store in the list playerGuesses
+        playerGuesses.add(letterGuess.charAt(0));
+    }
+
+    // If the player guess contains a character that is on the word, it gets replaced, else we print a dash
+    private static boolean printWordState(String word, List<Character> playerGuesses) {
+        int correctCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            if (playerGuesses.contains(word.charAt(i))) {
+            System.out.print(word.charAt(i));
+            correctCount++;
+            }
+            else {
+                System.out.print("-");
+            }
+        }
+        System.out.println();
+        return (word.length() == correctCount);
     }
 }
